f5933da99e5559d512108523198b46e21e75a66a
Arya Pasha Gunawan <aryapashaa49@gmail.com>
2023-03-24 15:05:35 +0700

    menambahkan layout admin

--- Client/wwwroot/js/employee.js
+++ Client/wwwroot/js/employee.js
@@ -1,33 +1,37 @@
-﻿//$.ajax({
-//    url: "https://pokeapi.co/api/v2/pokemon/"
-//}).done((result) => {
-//    console.log(result.results);
-//    let text = "";
-//    $.each(result.results, function (key, val) {
-//        text += `<tr>
-//                    <td>${key + 1}</td>
-//                    <td>${val.name}</td>
-//                    <td><button class="btn btn-primary" onclick="detail('${val.url}')" data-bs-toggle="modal" data-bs-target="#modalPoke">Detail</button></td>
-//                </tr>`;
-//        //console.log(key);
-//    })
-//    $("#tbodyEmployee").html(text)
+﻿function detail(nik, firstName, lastName, birthDate, gender, hiringDate, email, phoneNumber) {
+    let genderName = "";
+    if (gender == 0) {
+        genderName = "Laki-Laki";
+    } else {
+        genderName = "Perempuan";
+    };
+    let txt = `<li>Nik              : ${nik}</li>
+               <li>Nama Depan       : ${firstName}</li>
+               <li>Nama Belakang    : ${lastName}</li>
+               <li>Tanggal Lahir    : ${birthDate}</li>
+               <li>Jenis Kelamin    : ${genderName}</li>
+               <li>Tanggal Masuk    : ${hiringDate}</li>
+               <li>E-Mail           : ${email}</li>
+               <li>Handphone        : ${phoneNumber}</li>
+              `;
 
-//});
+    $(".modal-body").html(txt);
+    $("h1#exampleModalLabel.modal-title").html(firstName);
 
-function detail(stringUrl) {
-    $.ajax({
-        url: stringUrl
-    }).done((result) => {
-        console.log(result);
-        $("h1#exampleModalLabel.modal-title").html(result.data.firstName);
-    });
 }
 
 $(document).ready(function () {
     //initialisasi sekali saat HTML selesai di load
     $('#myTable').DataTable({
-        dom: 'Bfrtip',
+        //dom: '<"top"i>rt<"bottom"flp><"clear">',
+        dom: 'B<"clear">lfrtip',
+        buttons: [
+            { extend: 'copy', text: '<i class="fa fa-files-o"></i>', titleAttr: 'Copy', className: 'btn btn-outline-primary' },
+            { extend: 'csv', text: '<i class="fa fa-file-text-o"></i>', titleAttr: 'CSV', className: 'btn btn-outline-success' },
+            { extend: 'excel', text: '<i class="fa fa-file-excel-o"></i>', titleAttr: 'Excel', className: 'btn btn-outline-success' },
+            { extend: 'pdf', text: '<i class="fa fa-file-pdf-o"></i>', titleAttr: 'PDF', className: 'btn btn-outline-danger' },
+            { extend: 'print', text: '<i class="fa fa-print"></i>', titleAttr: 'Print', className: 'btn btn-outline-info' }
+        ],
         ajax: {
             url: "https://localhost:7205/api/Employees", //=> CORS
             dataSrc: "data",
@@ -48,14 +52,117 @@ $(document).ready(function () {
                 }
             },
             {
-                data: "email",
-                //render: function (data, type, row) {
-                //    `<button class="btn btn-primary" onclick="detail('${data}')" data-bs-toggle="modal" data-bs-target="#modalPoke">Detail</button>`;
-                //}
-            },
+                data: "",
+                render: function (data, type, row) {
+                    return `<button class="btn btn-primary" onclick="detail(
+                    '${row['nik']}', 
+                    '${row['firstName']}',
+                    '${row['lastName']}',
+                    '${row['birthDate']}', 
+                    '${row['gender']}',
+                    '${row['hiringDate']}',
+                    '${row['email']}',
+                    '${row['phoneNumber']}'
+                    )" data-bs-toggle="modal" data-bs-target="#modalEmployee">Detail</button>`;
+                }
+			},
+			//{
+			//	data: "",
+			//	render: function (data, type, row) {
+			//		return `<td>
+			//	<button type="button" class="btn btn-primary btn-xs dt-edit" style="margin-right:16px;">
+			//		<span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+			//	</button>
+			//	<button type="button" class="btn btn-danger btn-xs dt-delete">
+			//		<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
+			//	</button>
+			//</td>`;
+			//	}
+			//}
         ],
-        buttons: [
-            'copy', 'csv', 'excel', 'pdf', 'print'
-        ]
     });
+	//$('.dt-add').each(function () {
+	//	$(this).on('click', function (evt) {
+	//		//Create some data and insert it
+	//		var rowData = [];
+	//		var table = $('#example').DataTable();
+	//		//Store next row number in array
+	//		var info = table.page.info();
+	//		rowData.push(info.recordsTotal + 1);
+	//		//Some description
+	//		rowData.push('New Order');
+	//		//Random date
+	//		var date1 = new Date(2016, 01, 01);
+	//		var date2 = new Date(2018, 12, 31);
+	//		var rndDate = new Date(+date1 + Math.random() * (date2 - date1));//.toLocaleDateString();
+	//		rowData.push(rndDate.getFullYear() + '/' + (rndDate.getMonth() + 1) + '/' + rndDate.getDate());
+	//		//Status column
+	//		rowData.push('NEW');
+	//		//Amount column
+	//		rowData.push(Math.floor(Math.random() * 2000) + 1);
+	//		//Inserting the buttons ???
+	//		rowData.push('<button type="button" class="btn btn-primary btn-xs dt-edit" style="margin-right:16px;"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></button><button type="button" class="btn btn-danger btn-xs dt-delete"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button>');
+	//		//Looping over columns is possible
+	//		//var colCount = table.columns()[0].length;
+	//		//for(var i=0; i < colCount; i++){			}
+
+	//		//INSERT THE ROW
+	//		table.row.add(rowData).draw(false);
+	//		//REMOVE EDIT AND DELETE EVENTS FROM ALL BUTTONS
+	//		$('.dt-edit').off('click');
+	//		$('.dt-delete').off('click');
+	//		//CREATE NEW CLICK EVENTS
+	//		$('.dt-edit').each(function () {
+	//			$(this).on('click', function (evt) {
+	//				$this = $(this);
+	//				var dtRow = $this.parents('tr');
+	//				$('div.modal-body').innerHTML = '';
+	//				$('div.modal-body').append('Row index: ' + dtRow[0].rowIndex + '<br/>');
+	//				$('div.modal-body').append('Number of columns: ' + dtRow[0].cells.length + '<br/>');
+	//				for (var i = 0; i < dtRow[0].cells.length; i++) {
+	//					$('div.modal-body').append('Cell (column, row) ' + dtRow[0].cells[i]._DT_CellIndex.column + ', ' + dtRow[0].cells[i]._DT_CellIndex.row + ' => innerHTML : ' + dtRow[0].cells[i].innerHTML + '<br/>');
+	//				}
+	//				$('#myModal').modal('show');
+	//			});
+	//		});
+	//		$('.dt-delete').each(function () {
+	//			$(this).on('click', function (evt) {
+	//				$this = $(this);
+	//				var dtRow = $this.parents('tr');
+	//				if (confirm("Are you sure to delete this row?")) {
+	//					var table = $('#example').DataTable();
+	//					table.row(dtRow[0].rowIndex - 1).remove().draw(false);
+	//				}
+	//			});
+	//		});
+	//	});
+	//});
+	////Edit row buttons
+	//$('.dt-edit').each(function () {
+	//	$(this).on('click', function (evt) {
+	//		$this = $(this);
+	//		var dtRow = $this.parents('tr');
+	//		$('div.modal-body').innerHTML = '';
+	//		$('div.modal-body').append('Row index: ' + dtRow[0].rowIndex + '<br/>');
+	//		$('div.modal-body').append('Number of columns: ' + dtRow[0].cells.length + '<br/>');
+	//		for (var i = 0; i < dtRow[0].cells.length; i++) {
+	//			$('div.modal-body').append('Cell (column, row) ' + dtRow[0].cells[i]._DT_CellIndex.column + ', ' + dtRow[0].cells[i]._DT_CellIndex.row + ' => innerHTML : ' + dtRow[0].cells[i].innerHTML + '<br/>');
+	//		}
+	//		$('#myModal').modal('show');
+	//	});
+	//});
+	////Delete buttons
+	//$('.dt-delete').each(function () {
+	//	$(this).on('click', function (evt) {
+	//		$this = $(this);
+	//		var dtRow = $this.parents('tr');
+	//		if (confirm("Are you sure to delete this row?")) {
+	//			var table = $('#example').DataTable();
+	//			table.row(dtRow[0].rowIndex - 1).remove().draw(false);
+	//		}
+	//	});
+	//});
+	//$('#myModal').on('hidden.bs.modal', function (evt) {
+	//	$('.modal .modal-body').empty();
+	//});
 });
