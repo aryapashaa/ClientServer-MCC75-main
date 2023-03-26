5fc26959c5c53b2401c3aff6bc0dc6cf93bc3b23
Arya Pasha Gunawan <aryapashaa49@gmail.com>
2023-03-26 15:19:08 +0700

    menambahkan tooltip

--- Client/wwwroot/js/employee.js
+++ Client/wwwroot/js/employee.js
@@ -1,31 +1,56 @@
-﻿function detail(nik, firstName, lastName, birthDate, gender, hiringDate, email, phoneNumber) {
-    let genderName = "";
-    if (gender == 0) {
-        genderName = "Laki-Laki";
-    } else {
-        genderName = "Perempuan";
-    };
-    let txt = `<li>Nik              : ${nik}</li>
-               <li>Nama Depan       : ${firstName}</li>
-               <li>Nama Belakang    : ${lastName}</li>
-               <li>Tanggal Lahir    : ${birthDate}</li>
-               <li>Jenis Kelamin    : ${genderName}</li>
-               <li>Tanggal Masuk    : ${hiringDate}</li>
-               <li>E-Mail           : ${email}</li>
-               <li>Handphone        : ${phoneNumber}</li>
-              `;
-
-    $(".modal-body").html(txt);
-    $("h1#exampleModalLabel.modal-title").html(firstName);
-
-}
+﻿let editor;
 
 $(document).ready(function () {
+    //editor = new $.fn.datatable.editor({
+    //    "ajax": "https://localhost:7205/api/employees",
+    //    "table": "#mytable",
+    //    "fields": [{
+    //        "label": "nik:",
+    //        "name": "nik"
+    //    }, {
+    //        "label": "nama depan:",
+    //        "name": "firstname"
+    //    }, {
+    //        "label": "nama belakang:",
+    //        "name": "lastname"
+    //    }
+            //, {
+    //        "label": "Tanggal Lahir:",
+    //        "name": "birthDate",
+    //        "type": "datetime"
+    //    }, {
+    //        "label": "Jenis Kelamin:",
+    //        "name": "gender"
+    //    }, {
+    //        "label": "Tanggal Masuk:",
+    //        "name": "hiringDate",
+    //        "type": "datetime"
+    //    }, {
+    //        "label": "E-Mail:",
+    //        "name": "email"
+    //    }, {
+    //        "label": "Phone:",
+    //        "name": "phoneNumber"
+    //    }, {
+    //        "label": "Manager Id:",
+    //        "name": "managerId"
+    //    }
+    //    ]
+    //});
+
     //initialisasi sekali saat HTML selesai di load
     $('#myTable').DataTable({
         //dom: '<"top"i>rt<"bottom"flp><"clear">',
         dom: 'B<"clear">lfrtip',
         buttons: [
+            //{
+            //    text: '<i class="fa fa-plus" aria-hidden="true"></i>',
+            //    titleAttr: 'Add',
+            //    className: 'btn btn-success',
+            //    action: function (e, dt, node, config) {
+            //        alert('Button activated');
+            //    }
+            //},
             { extend: 'copy', text: '<i class="fa fa-files-o"></i>', titleAttr: 'Copy', className: 'btn btn-outline-primary' },
             { extend: 'csv', text: '<i class="fa fa-file-text-o"></i>', titleAttr: 'CSV', className: 'btn btn-outline-success' },
             { extend: 'excel', text: '<i class="fa fa-file-excel-o"></i>', titleAttr: 'Excel', className: 'btn btn-outline-success' },
@@ -54,7 +79,8 @@ $(document).ready(function () {
             {
                 data: "",
                 render: function (data, type, row) {
-                    return `<button class="btn btn-primary" onclick="detail(
+                    return `
+					<button class="btn btn-info dt-detail" onclick="detail(
                     '${row['nik']}', 
                     '${row['firstName']}',
                     '${row['lastName']}',
@@ -62,107 +88,141 @@ $(document).ready(function () {
                     '${row['gender']}',
                     '${row['hiringDate']}',
                     '${row['email']}',
-                    '${row['phoneNumber']}'
-                    )" data-bs-toggle="modal" data-bs-target="#modalEmployee">Detail</button>`;
+                    '${row['phoneNumber']}',
+                    '${row['managerId']}'
+                    )" data-bs-toggle="modal" data-bs-target="#modalDetail">
+						<a data-bs-toggle="tooltip" data-bs-placement="top" title="Detail">
+							<i class="fa fa-info-circle"></i>
+						</a>
+					</button>
+
+					<button class="btn btn-warning dt-edit" onclick="edit(
+					)" data-bs-toggle="modal" data-bs-target="#modalEdit">
+						<a data-bs-toggle="tooltip" data-bs-placement="top" title="Edit">
+							<i class="fa fa-edit"></i>
+						</a>
+					</button>
+
+					<button class="btn btn-danger dt-delete" onclick="delete(
+					)" data-bs-toggle="modal" data-bs-target="#modalDelete">
+						<a data-bs-toggle="tooltip" data-bs-placement="top" title="Delete">
+							<i class="fa fa-remove"></i>
+						</a>
+					</button>
+					`;
                 }
-			},
-			//{
-			//	data: "",
-			//	render: function (data, type, row) {
-			//		return `<td>
-			//	<button type="button" class="btn btn-primary btn-xs dt-edit" style="margin-right:16px;">
-			//		<span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
-			//	</button>
-			//	<button type="button" class="btn btn-danger btn-xs dt-delete">
-			//		<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
-			//	</button>
-			//</td>`;
-			//	}
-			//}
+            },
         ],
     });
-	//$('.dt-add').each(function () {
-	//	$(this).on('click', function (evt) {
-	//		//Create some data and insert it
-	//		var rowData = [];
-	//		var table = $('#example').DataTable();
-	//		//Store next row number in array
-	//		var info = table.page.info();
-	//		rowData.push(info.recordsTotal + 1);
-	//		//Some description
-	//		rowData.push('New Order');
-	//		//Random date
-	//		var date1 = new Date(2016, 01, 01);
-	//		var date2 = new Date(2018, 12, 31);
-	//		var rndDate = new Date(+date1 + Math.random() * (date2 - date1));//.toLocaleDateString();
-	//		rowData.push(rndDate.getFullYear() + '/' + (rndDate.getMonth() + 1) + '/' + rndDate.getDate());
-	//		//Status column
-	//		rowData.push('NEW');
-	//		//Amount column
-	//		rowData.push(Math.floor(Math.random() * 2000) + 1);
-	//		//Inserting the buttons ???
-	//		rowData.push('<button type="button" class="btn btn-primary btn-xs dt-edit" style="margin-right:16px;"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></button><button type="button" class="btn btn-danger btn-xs dt-delete"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button>');
-	//		//Looping over columns is possible
-	//		//var colCount = table.columns()[0].length;
-	//		//for(var i=0; i < colCount; i++){			}
-
-	//		//INSERT THE ROW
-	//		table.row.add(rowData).draw(false);
-	//		//REMOVE EDIT AND DELETE EVENTS FROM ALL BUTTONS
-	//		$('.dt-edit').off('click');
-	//		$('.dt-delete').off('click');
-	//		//CREATE NEW CLICK EVENTS
-	//		$('.dt-edit').each(function () {
-	//			$(this).on('click', function (evt) {
-	//				$this = $(this);
-	//				var dtRow = $this.parents('tr');
-	//				$('div.modal-body').innerHTML = '';
-	//				$('div.modal-body').append('Row index: ' + dtRow[0].rowIndex + '<br/>');
-	//				$('div.modal-body').append('Number of columns: ' + dtRow[0].cells.length + '<br/>');
-	//				for (var i = 0; i < dtRow[0].cells.length; i++) {
-	//					$('div.modal-body').append('Cell (column, row) ' + dtRow[0].cells[i]._DT_CellIndex.column + ', ' + dtRow[0].cells[i]._DT_CellIndex.row + ' => innerHTML : ' + dtRow[0].cells[i].innerHTML + '<br/>');
-	//				}
-	//				$('#myModal').modal('show');
-	//			});
-	//		});
-	//		$('.dt-delete').each(function () {
-	//			$(this).on('click', function (evt) {
-	//				$this = $(this);
-	//				var dtRow = $this.parents('tr');
-	//				if (confirm("Are you sure to delete this row?")) {
-	//					var table = $('#example').DataTable();
-	//					table.row(dtRow[0].rowIndex - 1).remove().draw(false);
-	//				}
-	//			});
-	//		});
-	//	});
-	//});
-	////Edit row buttons
-	//$('.dt-edit').each(function () {
-	//	$(this).on('click', function (evt) {
-	//		$this = $(this);
-	//		var dtRow = $this.parents('tr');
-	//		$('div.modal-body').innerHTML = '';
-	//		$('div.modal-body').append('Row index: ' + dtRow[0].rowIndex + '<br/>');
-	//		$('div.modal-body').append('Number of columns: ' + dtRow[0].cells.length + '<br/>');
-	//		for (var i = 0; i < dtRow[0].cells.length; i++) {
-	//			$('div.modal-body').append('Cell (column, row) ' + dtRow[0].cells[i]._DT_CellIndex.column + ', ' + dtRow[0].cells[i]._DT_CellIndex.row + ' => innerHTML : ' + dtRow[0].cells[i].innerHTML + '<br/>');
-	//		}
-	//		$('#myModal').modal('show');
-	//	});
-	//});
-	////Delete buttons
-	//$('.dt-delete').each(function () {
-	//	$(this).on('click', function (evt) {
-	//		$this = $(this);
-	//		var dtRow = $this.parents('tr');
-	//		if (confirm("Are you sure to delete this row?")) {
-	//			var table = $('#example').DataTable();
-	//			table.row(dtRow[0].rowIndex - 1).remove().draw(false);
-	//		}
-	//	});
-	//});
-	//$('#myModal').on('hidden.bs.modal', function (evt) {
-	//	$('.modal .modal-body').empty();
-	//});
 });
+
+// Detail
+function detail(nik, firstName, lastName, birthDate, gender, hiringDate, email, phoneNumber, managerId) {
+    let genderName = "";
+    if (gender == 0) {
+        genderName = "Laki-Laki";
+    } else {
+        genderName = "Perempuan";
+    };
+    let txt = `<li>NIK              : ${nik}</li>
+               <li>Nama Depan       : ${firstName}</li>
+               <li>Nama Belakang    : ${lastName}</li>
+               <li>Tanggal Lahir    : ${birthDate}</li>
+               <li>Jenis Kelamin    : ${genderName}</li>
+               <li>Tanggal Masuk    : ${hiringDate}</li>
+               <li>E-Mail           : ${email}</li>
+               <li>Handphone        : ${phoneNumber}</li>
+               <li>Manager ID       : ${managerId}</li>
+              `;
+
+    $(".modal-body").html(txt);
+    $("h1#modalLabelDetail.modal-title").html(firstName);
+}
+
+// Validation
+function validation() {
+    let txt = ` <form class="row g-3 needs-validation" novalidate>
+                  <div class="col-md-4 position-relative">
+                    <label for="validationTooltip01" class="form-label">First name</label>
+                    <input type="text" class="form-control" id="validationTooltip01" value="Mark" required>
+                    <div class="valid-tooltip">
+                      Looks good!
+                    </div>
+                  </div>
+                  <div class="col-md-4 position-relative">
+                    <label for="validationTooltip02" class="form-label">Last name</label>
+                    <input type="text" class="form-control" id="validationTooltip02" value="Otto" required>
+                    <div class="valid-tooltip">
+                      Looks good!
+                    </div>
+                  </div>
+                  <div class="col-md-4 position-relative">
+                    <label for="validationTooltipUsername" class="form-label">Username</label>
+                    <div class="input-group has-validation">
+                      <span class="input-group-text" id="validationTooltipUsernamePrepend">@</span>
+                      <input type="text" class="form-control" id="validationTooltipUsername" aria-describedby="validationTooltipUsernamePrepend" required>
+                      <div class="invalid-tooltip">
+                        Please choose a unique and valid username.
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-6 position-relative">
+                    <label for="validationTooltip03" class="form-label">City</label>
+                    <input type="text" class="form-control" id="validationTooltip03" required>
+                    <div class="invalid-tooltip">
+                      Please provide a valid city.
+                    </div>
+                  </div>
+                  <div class="col-md-3 position-relative">
+                    <label for="validationTooltip04" class="form-label">State</label>
+                    <select class="form-select" id="validationTooltip04" required>
+                      <option selected disabled value="">Choose...</option>
+                      <option>...</option>
+                    </select>
+                    <div class="invalid-tooltip">
+                      Please select a valid state.
+                    </div>
+                  </div>
+                  <div class="col-md-3 position-relative">
+                    <label for="validationTooltip05" class="form-label">Zip</label>
+                    <input type="text" class="form-control" id="validationTooltip05" required>
+                    <div class="invalid-tooltip">
+                      Please provide a valid zip.
+                    </div>
+                  </div>
+                  <div class="col-12">
+                    <button class="btn btn-primary" onclick="insert(
+					)" type="submit">Submit</button>
+                  </div>
+                </form>
+              `;
+
+    $(".modal-body").html(txt);
+}
+
+// Insert
+function insert() {
+    var obj = new Object(); //sesuaikan sendiri nama objectnya dan beserta isinya
+    //ini ngambil value dari tiap inputan di form nya
+    obj.Nik = $("#nik").val();
+    obj.FirstName = $("#firstName").val();
+    obj.LastName = $("#lastName").val();
+    obj.BirthDate = $("#birthDate").val();
+    obj.Gender = $("#gender").val();
+    obj.HiringDate = $("#hiringDate").val();
+    obj.Email = $("#email").val();
+    obj.Handphone = $("#phoneNumber").val();
+    obj.ManagerId = $("managerId").val();
+
+    //isi dari object kalian buat sesuai dengan bentuk object yang akan di post
+    $.ajax({
+        url: "https://localhost:7205/api/Employees",
+        type: "POST",
+        dataType: "JSON",
+        data: JSON.stringify(obj) //jika terkena 415 unsupported media type (tambahkan headertype Json & JSON.Stringify();)
+    }).done((result) => {
+        alert('Data Insert Successfully');
+    }).fail((error) => {
+        alert('Data Insert Failed');
+    })
+}
